66fdcc87f4680e101d84f8718d482ab4e8e68418
Doguhan Akgunes <137198411+dovuhan@users.noreply.github.com>
2023-10-17 15:35:04 +0300

    Update port-scan.py
    Bu sürümde PortScanner'a bazı önemli özellikler ve geliştirmeler ekledik.

--- port-scan.py
+++ port-scan.py
@@ -2,16 +2,22 @@ import socket
 from ipaddress import IPv4Network
 import concurrent.futures
 
+def get_service(port):
+    """Port numarasına göre yaygın servis adını döndürür."""
+    service_name = socket.getservbyport(port, 'tcp')
+    return service_name or 'Unknown'
+
 
 def scan_port(ip, port):
     """Bir IP'deki belirli bir portun açık olup olmadığını kontrol eder."""
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.settimeout(1)  # 1 saniyelik zaman aşımı
+        s.settimeout(1)
         try:
             s.connect((ip, port))
-            return port, True
+            service = get_service(port)
+            return port, True, service
         except:
-            return port, False
+            return port, False, ''
 
 
 def scan_ports_for_ip(ip, ports):
@@ -21,9 +27,9 @@ def scan_ports_for_ip(ip, ports):
         results = {executor.submit(scan_port, ip, port): port for port in ports}
 
         for future in concurrent.futures.as_completed(results):
-            port, status = future.result()
+            port, status, service = future.result()
             if status:
-                open_ports.append(port)
+                open_ports.append((port, service))
 
     return open_ports
 
@@ -34,11 +40,14 @@ def main():
     end_port = int(input("Bitiş portunu girin: "))
 
     ports_to_scan = list(range(start_port, end_port + 1))
+    all_ips = list(IPv4Network(ip_range, strict=False))
 
-    for ip in IPv4Network(ip_range, strict=False):
+    for index, ip in enumerate(all_ips, 1):
+        print(f"{index}/{len(all_ips)} - {ip} taranıyor...")
         open_ports = scan_ports_for_ip(str(ip), ports_to_scan)
         if open_ports:
-            print(f"{ip} üzerindeki açık portlar: {', '.join(map(str, open_ports))}")
+            port_strings = [f"{port} ({service})" for port, service in open_ports]
+            print(f"{ip} üzerindeki açık portlar: {', '.join(port_strings)}")
 
 
 if __name__ == "__main__":
